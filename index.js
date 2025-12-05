8da2352b2a203630206caab2d365e899f023c0fc
CHANDAN <kumardalaichandan803@gmail.com>
2025-12-05 23:21:18 +0530

    Add files via upload
    Using Node.js v18+, install the Google Gen AI SDK for TypeScript and JavaScript using the following npm command:
    
    
    npm install @google/genai

---
+++ index.js
@@ -0,0 +1,32 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({apiKey:"AIzaSyCSZNXWJTjwkuNSnXSFYe3B_6W2PGsibvA"});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: [
+        {
+            role:"user",
+            parts:[{text:"What's the worst reply to I love you? i need rost version replay to this question with in   in 10 word "}]
+        },
+        
+
+
+
+
+        
+    //   {
+    //     role: "user",
+    //     parts: [{ text: "What is my name?" }]
+    //   },
+    //   {
+    //     role: "user",
+    //     parts: [{ text: "My name is Chandan. Nice to meet you." }]
+    //   }  
+    ],
+  });
+  console.log(response.text);
+}
+
+await main();
